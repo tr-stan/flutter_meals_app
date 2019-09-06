69d5d828b1f5e4c6d4b41d701397a3c45e65500d
Tristan Bennett <betriistan@gmail.com>
2019-09-06 12:13:36 -0500

    changes tabs bar from top- to bottom-of-page navigation

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,26 +8,84 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  // list of widgets to be included in the navbar tabs
+  final List<Map<String, dynamic>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    },
+  ];
+
+  // variable for tracking which tab is selected, default first tab
+  int _selectedPageIndex = 0;
+
+  // func for the onTap of the bottom tab bar nav
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0, // could be set to 1 to make 2nd tab the default
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.category), text: 'Categories'),
-              Tab(icon: Icon(Icons.star), text: 'Favorites'),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      // output specific screen depending on index from _selectPage onTap method
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Theme.of(context).primaryColorLight,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // selectedFontSize & unSelectedFontSize already have default values
+        // selectedFontSize: 20,
+        // unselectedFontSize: 16,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            // with type: BottomNavigtionBarType.shifting you have to
+            // set the backgroundColor of each individual navbar item
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavoritesScreen(),
-        ],),
+          BottomNavigationBarItem(
+            // with type: BottomNavigtionBarType.shifting you have to
+            // set the backgroundColor of each individual navbar item
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
+
+    // Below is the Tab controller for a tab bar at the top of the screen
+    // DefaultTabController(
+    //   length: 2,
+    //   initialIndex: 0, // could be set to 1 to make 2nd tab the default
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Meals'),
+    //       bottom: TabBar(
+    //         tabs: <Widget>[
+    //           Tab(icon: Icon(Icons.category), text: 'Categories'),
+    //           Tab(icon: Icon(Icons.star), text: 'Favorites'),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(children: <Widget>[
+    //       CategoriesScreen(),
+    //       FavoritesScreen(),
+    //     ],),
+    //   ),
+    // );
   }
 }
