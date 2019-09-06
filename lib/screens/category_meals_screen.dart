9a946a4e379afc6a0aa2734bb02a4f9eb28ff66c
Tristan Bennett <betriistan@gmail.com>
2019-09-06 14:45:37 -0500

    adds temp remove logic for category_meals_screen list items

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -2,35 +2,61 @@ import 'package:flutter/material.dart';
 
 import '../dummy_data.dart';
 import '../widgets/meal_item.dart';
+import '../models/meal.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
+
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+
+  // didChangeDependencies runs after initState and can capture the context
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     // take args provided by pushNamed method
-    final routeArgs =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryTitle = routeArgs['title'];
-    final categoryId = routeArgs['id'];
-    final filteredMeals = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
 
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
       body: ListView.builder(
         itemBuilder: (context, index) {
           return MealItem(
-            id: filteredMeals[index].id,
-            title: filteredMeals[index].title,
-            imageUrl: filteredMeals[index].imageUrl,
-            duration: filteredMeals[index].duration,
-            affordability: filteredMeals[index].affordability,
-            complexity: filteredMeals[index].complexity,
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            duration: displayedMeals[index].duration,
+            affordability: displayedMeals[index].affordability,
+            complexity: displayedMeals[index].complexity,
+            removeItem: _removeMeal,
           );
         },
-        itemCount: filteredMeals.length,
+        itemCount: displayedMeals.length,
       ),
     );
   }
--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -44,61 +44,69 @@ class MealDetailsScreen extends StatelessWidget {
         title: Text('${selectedMeal.title}'),
       ),
       body: SafeArea(
-              child: SingleChildScrollView(
-                  child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  child: Image.network(
-                    selectedMeal.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
                 ),
-                buildSectionTitle(context, 'Ingredients'),
-                buildContainer(
-                  context,
-                  ListView.builder(
-                    itemBuilder: (ctx, index) => Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 10,
-                      ),
-                      child: Card(
-                        color: Theme.of(context).accentColor,
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Text(selectedMeal.ingredients[index]),
-                        ),
+              ),
+              buildSectionTitle(context, 'Ingredients'),
+              buildContainer(
+                context,
+                ListView.builder(
+                  itemBuilder: (ctx, index) => Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Card(
+                      color: Theme.of(context).accentColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(selectedMeal.ingredients[index]),
                       ),
                     ),
-                    itemCount: selectedMeal.ingredients.length,
                   ),
+                  itemCount: selectedMeal.ingredients.length,
                 ),
-                buildSectionTitle(context, 'Steps'),
-                buildContainer(
-                  context,
-                  ListView.builder(
-                    itemBuilder: (ctx, index) => Column(
-                      children: <Widget>[
-                        ListTile(
-                          leading: CircleAvatar(
-                            child: Text('# ${(index + 1)}'),
-                          ),
-                          title: Text(
-                            selectedMeal.steps[index],
-                          ),
+              ),
+              buildSectionTitle(context, 'Steps'),
+              buildContainer(
+                context,
+                ListView.builder(
+                  itemBuilder: (ctx, index) => Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('# ${(index + 1)}'),
                         ),
-                        Divider(),
-                      ],
-                    ),
-                    itemCount: selectedMeal.steps.length,
+                        title: Text(
+                          selectedMeal.steps[index],
+                        ),
+                      ),
+                      Divider(),
+                    ],
                   ),
+                  itemCount: selectedMeal.steps.length,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.delete,
+        ),
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
       ),
     );
   }
